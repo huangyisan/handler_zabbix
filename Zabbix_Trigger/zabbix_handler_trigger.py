826e23ec86d3864aa34c6f1cfa6fc57e068b193b
huangyisan <anonymousyisan@gmail.com>
2019-01-04 09:30:08 +0800

    rename trigger

---
+++ Zabbix_Trigger/zabbix_handler_trigger.py
@@ -0,0 +1,11 @@
+from Zabbix_JSRPC.zabbix_jsrpc_query import JSRPCQuery
+from Zabbix_Payloads import zabbix_triggers_info
+
+class ZabbixTrigger(JSRPCQuery):
+
+    def get_count_trigger(self,payload):
+        return self.zabbix_jsrpc_query(payload).get("result","")
+
+
+c = ZabbixTrigger()
+print(c.get_count_trigger(payload=zabbix_triggers_info.all_alerts_payload))
