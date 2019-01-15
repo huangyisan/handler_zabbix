7bf0a754bda4c7d401c9a641c32d19a4b0f8e0db
huangyisan <anonymousyisan@gmail.com>
2019-01-15 18:05:02 +0800

    update

--- Zabbix_Hosts/zabbix_handler_hosts.py
+++ Zabbix_Hosts/zabbix_handler_hosts.py
@@ -4,8 +4,17 @@ from Zabbix_Payloads import zabbix_hosts_info
 
 class ZabbixHosts(JSRPCQuery):
 
-    def get_all_hosts(self,payload):
-        return self.zabbix_jsrpc_query(payload).get("result","")
+    def _get_hosts(self,payload):
+        return self.zabbix_jsrpc_query(payload).get("result", "")
+
+    def get_all_hosts(self):
+        '''
+
+        :param output_data:
+        :return:
+        '''
+        payload = zabbix_hosts_info.get_all_hosts_payload("extend")
+        return self._get_hosts(payload)
 
     def get_customer_info(self,output_data):
         '''
@@ -14,8 +23,9 @@ class ZabbixHosts(JSRPCQuery):
         :return:
         '''
         payload = zabbix_hosts_info.get_all_hosts_payload(output_data)
-        return self.get_all_hosts(payload)
+        return self._get_hosts(payload)
 
 # c = ZabbixHosts()
 # output_data = ["hostid","host"]
-# print(c.get_custmer_info(output_data=output_data))
+# # print(c.get_customer_info(output_data=output_data))
+# print(c.get_all_hosts())
